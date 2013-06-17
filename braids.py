a92935a687b29e2a3533cabb47938dfc3b92c425
seshu yamajala <syamajala@gmail.com>
2013-06-16 23:47:32 -0400

    Some work on phi_LB and phi_RB

--- braids.py
+++ braids.py
@@ -4,9 +4,9 @@ from sage.all import *
 
 class Braid:
 
-    def __init__(self, strands, crossings):
+    def __init__(self, strands, word):
         self.s = strands
-        self.c = crossings
+        self.w = word
         self.alg = FreeAlgebra(ZZ, (self.s+1)*(self.s+1), self.aij(star=True))
         self.alg.inject_variables()
 
@@ -43,10 +43,6 @@ class Braid:
                     a = a + 'a'+str(i)+str(j)
                 a = a + ','
 
-        # for i in range(start, self.s+1):
-        #     for j in range(start, self.s+1):
-        #         a = a +'a'+str(i)+str(j)+','
-
         return a[:len(a)-1]
 
     def phi_s(self, z, x, y):
@@ -54,48 +50,128 @@ class Braid:
         i = x - 1
         j = y - 1
 
+        if i == -1 and j == -1:
+            return [(-1, -1)]
+
         if (i == k and j == k) or (i == k+1 and j == k+1):
-            return 0
+            return [(-1, -1)]
         elif i == k + 1:
             if j != k and j != k + 1:
-                return self.a[k][j]
+                return [(k, j)]
             else:
-                return self.a[k, k+1]
+                return [(k, k+1)]
         elif j == k + 1:
             if i != k and i != k + 1:
-                return self.a[i][k]
+                return [(i, k)]
             else:
-                return self.a[k+1][k]
+                return [(k+1, k)]
         elif i == k and j != k and j != k + 1:
-            return -1*self.a[k+1][j]-self.a[k+1][k]*self.a[k][j]
+            return [(k+1, j), (k+1, k), (k, j)]
         elif j == k and i != k and i != k + 1:
-            return -1*self.a[i][k+1]-self.a[i][k]*self.a[k][k+1]
+            return [(i, k+1), (i, k), (k, k+1)]
         elif i != k and i != k+1 and j != k and j != k+1:
-            return self.a[i][j]
+            return [(i, j)]
 
     def phi_ext(self, z, x, y):
         k = z
         i = x
         j = y 
 
+        if i == -1 and j == -1:
+            return [(-1, -1)]
+
         if (i == k and j == k) or (i == k+1 and j == k+1) or (i == 0 and j == 0):
-            return 0
+            return [(-1, -1)]
         elif i == k + 1:
             if j != k and j != k + 1:
-                return self.astar[k][j]
+                return [(k, j)]
             else:
-                return self.astar[k, k+1]
+                return [(k, k+1)]
         elif j == k + 1:
             if i != k and i != k + 1:
-                return self.astar[i][k]
+                return [(i, k)]
             else:
-                return self.astar[k+1][k]
+                return [(k+1, k)]
         elif i == k and j != k and j != k + 1:
-            return -1*self.astar[k+1][j]-self.astar[k+1][k]*self.astar[k][j]
+            return [(k+1, j), (k+1, k), (k, j)]
         elif j == k and i != k and i != k + 1:
-            return -1*self.astar[i][k+1]-self.astar[i][k]*self.astar[k][k+1]
+            return [(i, k+1), (i, k), (k, k+1)]
         elif i != k and i != k+1 and j != k and j != k+1:
-            return self.astar[i][j]
+            return [(i, j)]
+
+    def phi_b(self):
+        a = [[]]
+
+        for i in range(0, self.s+1):
+            for j in range(0, self.s+1):
+                a[0].append(self.phi_ext(self.w[0], i, j))
+
+        idx = 0
+        for k in self.w[1:]:
+            a.append([])
+            for i in a[idx]:
+                print i
+
+    def phi_b_help(self, w, l, r, n):
+
+        print
+
+        print "n = ",
+        print n
+        print "l = ",
+        print l
+
+        print "r = ",
+        print r
+
+        if l == [(-1, -1)]:
+            return l
+        elif l == []:
+            print "n = " + str(n) + " returning ",
+            print r
+            return r
+        else:
+            if type(l[0]) == list:
+                a = self.phi_b_help(w, l[0], [], n+1)
+                print "list n = " + str(n) + " a = ",
+                print a
+            else:
+                a = self.phi_ext(w, *l[0])
+                print "el n = " + str(n) + " a = ",
+                print a
+            if len(a) == 1:
+                r.extend(a)
+            else:
+                r.append(a)
+            self.phi_b_help(w, l[1:], r, n+1)
+
+    def phi_ls(self, i):
+        s = i-1
+        m = identity_matrix(self.alg, self.s)
+        m[s:s+2, s:s+2] = [[-1*self.a[s+1, s], -1], [1, 0]]
+        return m
+
+    def phi_rs(self, i):
+        s = i-1
+        m = identity_matrix(self.alg, self.s)
+        m[s:s+2, s:s+2] = [[-1*self.a[s, s+1], 1], [-1, 0]]
+        return m
+
+    def phi_lb(self):
+        m = self.phi_ls(self.w[0])
+
+        for i in self.w[1:]:
+            m = m*self.phi_ls(i)
+
+        return m
+
+    def phi_rb(self):
+        m = self.phi_rs(self.w[0])
+        
+        for i in self.w[1:]:
+            m = m*self.phi_rs(i)
+
+        return m
 
 #b = Braid(4, 13)
 # print
@@ -110,13 +186,13 @@ class Braid:
 #             print "=",
 #             print (b.phi_s(k, i, j))
 
-braid = Braid(2, 3)
-print
-print braid.astar
-print
+braid = Braid(2, [1, 1, 1])
+# print
+# print braid.astar
+# print
 
-for i in range(0, 3):
-    for j in range(0, 3):
-        print (i, j),
-        print "=",
-        print (braid.phi_ext(1, i, j))
+# for i in range(0, 3):
+#     for j in range(0, 3):
+#         print (i, j),
+#         print "=",
+#         print (braid.phi_ext(1, i, j))
